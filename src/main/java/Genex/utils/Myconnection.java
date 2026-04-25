481cf4f860d3188a9d37363fa62cd64638f52601
moham <MohamedAziz.Jarraya@esprit.tn>
2026-04-25 14:50:41 +0100

    Myconnection with singleton pattern and pom.xml dependencies

--- src/main/java/Genex/utils/Myconnection.java
+++ src/main/java/Genex/utils/Myconnection.java
@@ -1,4 +1,37 @@
 package Genex.utils;
 
+import java.sql.Connection;
+
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Myconnection {
+    private static Myconnection Instance;
+
+    private String url="jdbc:mysql://localhost:";
+    private String login="root";
+    private String pwd="";
+
+    private  Connection cnx;
+    public Connection getCnx() {
+        return cnx;
+    }
+
+    private Myconnection(){
+        try {
+            cnx=DriverManager.getConnection(url,login,pwd);
+            System.out.println("connection établie !");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
+
+    public static Myconnection getInstance (){
+        if (Instance==null)
+        {
+            Instance=new Myconnection();
+        }
+        return Instance;
+    }
 }
