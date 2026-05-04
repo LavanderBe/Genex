cce8f091fa5af002102b1bc13b90dc9b48f54673
DorghamOthmani <Dorgham.Othmani@esprit.tn>
2026-05-04 14:01:41 +0100

    fixes dorgham

---
+++ src/main/java/Genex/tests/test.java
@@ -0,0 +1,13 @@
+package Genex.tests;
+
+import Genex.entities.User;
+import Genex.services.CrudUser;
+
+public class test {
+    public static void main(String[] args) {
+        User u=new User ("user","mail@mail.com","mdp","admin");
+        CrudUser cu=new CrudUser();
+        cu.addEntity(u);
+    }
+
+}
