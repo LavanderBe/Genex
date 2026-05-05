72a322485c67ad249f8e64c18e0e4713e5b3a679
moham <MohamedAziz.Jarraya@esprit.tn>
2026-05-05 22:33:03 +0100

    fix another

--- src/main/java/Genex/utils/EmailSystem.java
+++ src/main/java/Genex/utils/EmailSystem.java
@@ -5,8 +5,7 @@ import com.sendgrid.helpers.mail.objects.*;
 import java.io.IOException;
 
 public class EmailSystem {
-    private static final String API_KEY = "";
-    ///private static final String API_KEY = System.getenv("SENDGRID_API_KEY");
+    private static final String API_KEY = System.getenv("SENDGRID_API_KEY");
     private static String TEMPLATE_ID = "d-dce24ed71c034fe2972457aaaf380772"; // Paste your Template ID here
     private static final String FROM_EMAIL = "genexesportstn@gmail.com"; // Your verified sender
 
